f929aacec16286777fd32c8eb3203fa648db91dc
Михайлов Алексей <inert32@gmail.com>
2023-10-04 20:10:38 +0500

    CMakeLists: Добавлены файлы одиночного режима point.h: Добавлен класс point main.cpp: Подготовка к добавлению одиночного режима

---
+++ src/point.h
@@ -0,0 +1,36 @@
+#ifndef __POINT_H__
+#define __POINT_H__
+
+class point {
+public:
+    point() = default;
+    point(float _x, float _y, float _z) {
+        x = _x; y = _y; z = _z;
+    }
+    point(const point& old) {
+        x = old.x, y = old.y, z = old.z;
+    }
+    point(const point& from, const point& to) {
+        x = to.x - from.x, y = to.y - from.y, z = to.z - from.z;
+    }
+
+    inline point operator+(point p) const {
+        return { x + p.x, y + p.y, z + p.z };
+    }
+    inline point operator-(point p) const {
+        return { x - p.x, y - p.y, z - p.z };
+    }
+    inline void operator=(point p) {
+        x = p.x; y = p.y; z = p.z;
+    }
+
+    float x = 0.0, y = 0.0, z = 0.0;
+};
+
+struct triangle {
+    point A;
+    point B;
+    point C;
+};
+
+#endif /* __POINT_H__ */
---
+++ src/solo.h
@@ -0,0 +1,14 @@
+#ifndef __SOLO_H__
+#define __SOLO_H__
+
+/* Необходимые определения для одиночного режима */
+/* Парсер входного файла заданий, вычисления, запись результатов */
+
+#include <cmath>
+#include "point.h"
+
+class parser;
+
+void solo_start();
+
+#endif /* __SOLO_H__ */
